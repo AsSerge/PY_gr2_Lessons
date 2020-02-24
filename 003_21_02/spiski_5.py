e44e899200d899e0102a0a7ae03eccdd5092bac6
Serge Tsvetkov <z00m.serge@gmail.com>
2020-02-24 14:33:38 +0300

    Test2

--- 003_21_02/spiski_5.py
+++ 003_21_02/spiski_5.py
@@ -1,6 +1,6 @@
 ListNumber = [12,34,65,34,76,23,89,54,23,65,89,10,23,1,32,4,5,7,8,12,34]
 LString = [str(i) for i in ListNumber]
 LString = " ".join(LString) 
-print ("_Исходный список:", LString)
+print ("Исходный список:", LString)
 print ("Наибольшее значение в списке =", max(ListNumber))
 print ("Индекс первого вхождения наибольшего значения:", ListNumber.index(max(ListNumber)))
