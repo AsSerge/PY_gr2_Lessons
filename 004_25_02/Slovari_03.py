4237d6750b74f5b363a6553574b9102f8d0483ca
AsSerge <z00m.serge@gmail.com>
2020-02-26 15:10:49 +0300

    Test from Lermontov (Zadanie 7.3 - GOOD)

--- 004_25_02/Slovari_03.py
+++ 004_25_02/Slovari_03.py
@@ -5,16 +5,30 @@ with open('product_list.csv', 'r', encoding = 'UTF-8') as fp:
     reader = csv.reader(fp, delimiter=';', quotechar='"')
     data_read = [row for row in reader]
 
-for i in data_read: print (i)
-
 # Создаем новое множество покупателей (значения  - уникальны)
-Buyers = set()
-for k in data_read:
-    t = k[0]
-    Buyers.update( t )
-print(Buyers)
+Buyer = set()
+for i in data_read: Buyer.add(i[0])
+Buyer = sorted(list(Buyer)) # сортируем список покупателей по алфавиту
 
+# Выводим список покупателей и купленны х ими продукутов
+print("*"*40)
+for BuyerMame in Buyer:
+    print("\tПокупатель:",BuyerMame)
+    GoodsArr = {} # Создаем список продуктов
+    for Purchase in data_read:
+        if Purchase[0] == BuyerMame:
+            Goods = Purchase[1]
+            GoodsNumber = int(Purchase[2])
+            # Заполняем список
+            if not Goods in GoodsArr:
+                GoodsArr.update( {Goods:GoodsNumber} )
+            else:
+                GoodsNumber = int(GoodsArr[Goods]) + int(Purchase[2])
+                GoodsArr.update ( {Goods : GoodsNumber} )
 
-
-
-
+    # Здесь выводим отсортированный по ключу список товаров для каждого покупателя
+    list_key = list ( GoodsArr.keys () )
+    list_key.sort ()
+    for i in list_key :
+        print ( "\t", i , ":" , GoodsArr[i] )
+    print("*"*40)
