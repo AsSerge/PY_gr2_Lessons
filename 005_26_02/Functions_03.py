2494166fef95d5a8e6ff6ef241d56b170ae14399
AsSerge <z00m.serge@gmai.com>
2020-02-29 13:51:31 +0300

    Work 8 (map test)

--- 005_26_02/Functions_03.py
+++ 005_26_02/Functions_03.py
@@ -1,6 +1,18 @@
 # Задание 3
 print("Сумма квадратов всех четных двузначных чисел:\t",sum(map(lambda x: x ** 2, filter(lambda x: x % 2 == 0, range(10, 100)))))
 
+# Сравнение
+MyKey = print(">>>>>", sum(list(filter(lambda k: k % 4 == 0, range(10, 100)))))
+Mk = []
+for k in range(10, 100):
+    if k % 4 == 0:
+        Mk.append(k)
+print (">>>>>", sum(Mk))
+
+
+
+
+
 
 
 
